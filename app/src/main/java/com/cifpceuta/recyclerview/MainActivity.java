2f4b97777bb8d4677b264c50cf8f66f3d43e7210
kevinRodriguezCastro <10208@cifpceuta.es>
2023-11-24 21:12:32 +0100

    añadir

--- app/src/main/java/com/cifpceuta/recyclerview/MainActivity.java
+++ app/src/main/java/com/cifpceuta/recyclerview/MainActivity.java
@@ -8,12 +8,18 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 //import com.google.android.material.search.SearchView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -26,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
     private boolean ordAsc = true;
     private boolean modoLista = true;
     private SearchView barraBusqueda;
+
+    private FloatingActionButton fabAñadir;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +87,46 @@ public class MainActivity extends AppCompatActivity {
             public boolean onQueryTextChange(String texto){
                 filtrado(texto);
                 return false;
+            }
+        });
+
+        fabAñadir = findViewById(R.id.fabAñadir);
+        fabAñadir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Creamos el DIALOG asociado al ActivityMAIN
+                Dialog dialog = new Dialog(MainActivity.this);
+
+                //Le asociamos el layout correspondiente
+                dialog.setContentView(R.layout.dialog_layout);
+
+                //Recuperamos los views dentro de dicho layout para recuperar sus valores posteriormente
+                EditText etTextoItem = dialog.findViewById(R.id.etTextoItem);
+                Button btnDialogAñadir = dialog.findViewById(R.id.btnDialogAñadir);
+
+
+                //Establecemos el listener para capturar datos y realizar acción de añadir
+                btnDialogAñadir.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String item = etTextoItem.getText().toString();
+                        // tb se puede realizar una validación de campo no vacio...
+                        list_items.add(item);
+                        //añadimos el item al final
+                        adapter.notifyItemInserted(list_items.size()-1);
+
+                        //hacemos desplazar la lista de items hasta dicho valor
+                        recyclerView.scrollToPosition(list_items.size()-1);
+
+                        //Esta llamada cierra el dialogo
+                        dialog.dismiss();
+                    }
+                });
+
+
+                //Esta llamada abre el diálogo
+                dialog.show();
+
             }
         });
     }
